4eaf37e9ff901cab033685a4a6bf39065a901eb0
Alex Canario <alexcanario@gmail.com>
2021-07-18 01:15:39 -0300

    First commit

---
+++ TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("397f3646-f31a-4573-8c4e-31d80a950325")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests {
+    [TestClass]
+    public class ReservationTests {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue() {
+            //CanBeCancelledBy_Scenario_ExpectedBehavior
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CaBeCancelledBy_SameUserCancellingTheReservation_ReturnsFalse() {
+            var reservation = new Reservation();
+
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = false });
+
+            Assert.IsFalse(result);
+        }
+    }
+}
